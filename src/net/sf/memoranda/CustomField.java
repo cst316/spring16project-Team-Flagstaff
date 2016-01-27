9be4b1e84a8169ba1bfffb7a8bc025318ab3eab3
Galen Goforth <ghgoforth@asu.edu>
2016-01-27 00:44:01 -0700

    additional files added for task template

---
+++ src/net/sf/memoranda/CustomField.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package net.sf.memoranda;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+/**
+ * @author ggoforth
+ *
+ */
+public class CustomField {
+	public static HashMap<Integer, String> typeMap = new HashMap<Integer, String>();
+	typeMap.put(3, "Date");
+	typeMap.put(1, "Integer");
+	typeMap.put(2, "String");
+}
