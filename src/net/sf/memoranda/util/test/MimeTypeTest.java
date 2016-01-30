9b9c654f71ee53f483c29729c3e5f8157d212757
Galen Goforth <ghgofort@asu.edu>
2016-01-30 11:39:02 -0800

    initial commit for net.sf.memoranda.util.test This is to create frame work for testing the .until file ... (Need to add JUnit 4 along with JRE 1.8 in order to run)

---
+++ src/net/sf/memoranda/util/test/MimeTypeTest.java
@@ -0,0 +1,264 @@
+/**
+ * 
+ */
+package net.sf.memoranda.util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Kevin
+ *
+ */
+public class MimeTypeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#MimeType(Element)}.
+	 */
+	@Test
+	public void testMimeTypeElement() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#MimeType()}.
+	 */
+	@Test
+	public void testMimeType() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getMimeTypeId()}.
+	 */
+	@Test
+	public void testGetMimeTypeId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getExtension()}.
+	 */
+	@Test
+	public void testGetExtension() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getExtensions()}.
+	 */
+	@Test
+	public void testGetExtensions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#addExtension(java.lang.String)}.
+	 */
+	@Test
+	public void testAddExtension() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getLabel()}.
+	 */
+	@Test
+	public void testGetLabel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#setLabel(java.lang.String)}.
+	 */
+	@Test
+	public void testSetLabel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getAppId(java.lang.String)}.
+	 */
+	@Test
+	public void testGetAppIdString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#setApp(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testSetAppStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#setApp(java.lang.String)}.
+	 */
+	@Test
+	public void testSetAppString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getAppId()}.
+	 */
+	@Test
+	public void testGetAppId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getIconPath()}.
+	 */
+	@Test
+	public void testGetIconPath() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#setIconPath(java.lang.String)}.
+	 */
+	@Test
+	public void testSetIconPath() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.MimeType#getIcon()}.
+	 */
+	@Test
+	public void testGetIcon() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/net/sf/memoranda/util/test/UtilTest.java
@@ -0,0 +1,248 @@
+/**
+ * 
+ */
+package net.sf.memoranda.util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Kevin
+ *
+ */
+public class UtilTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#generateId()}.
+	 */
+	@Test
+	public void testGenerateId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getDateStamp(java.util.Calendar)}.
+	 */
+	@Test
+	public void testGetDateStampCalendar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getDateStamp(net.sf.memoranda.date.CalendarDate)}.
+	 */
+	@Test
+	public void testGetDateStampCalendarDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#parseDateStamp(java.lang.String)}.
+	 */
+	@Test
+	public void testParseDateStamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getEnvDir()}.
+	 */
+	@Test
+	public void testGetEnvDir() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getCDATA(java.lang.String)}.
+	 */
+	@Test
+	public void testGetCDATA() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#runBrowser(java.lang.String)}.
+	 */
+	@Test
+	public void testRunBrowser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#checkBrowser()}.
+	 */
+	@Test
+	public void testCheckBrowser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getHoursFromMillis(long)}.
+	 */
+	@Test
+	public void testGetHoursFromMillis() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getMillisFromHours(java.lang.String)}.
+	 */
+	@Test
+	public void testGetMillisFromHours() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#getTempFile()}.
+	 */
+	@Test
+	public void testGetTempFile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#debug(java.lang.String)}.
+	 */
+	@Test
+	public void testDebug() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.util.test.Util#error(java.lang.Exception)}.
+	 */
+	@Test
+	public void testError() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
