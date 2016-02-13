8f782e498f682e2d77852a743e942d474dd61531
Kevin Bryant <kjbryan1@asu.edu>
2016-02-13 09:54:15 -0800

    Initial commit for UI JUnit tests

---
+++ src/net/sf/memoranda/ui/test/AllFilesFilterTest.java
@@ -0,0 +1,192 @@
+/**
+ * 
+ */
+package net.sf.memoranda.ui.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Kevin
+ *
+ */
+public class AllFilesFilterTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.ui.test.AllFilesFilter#AllFilesFilter(java.lang.String)}.
+	 */
+	@Test
+	public void testAllFilesFilter() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.ui.test.AllFilesFilter#accept(java.io.File)}.
+	 */
+	@Test
+	public void testAcceptFile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.ui.test.AllFilesFilter#getDescription()}.
+	 */
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.filechooser.FileFilter#FileFilter()}.
+	 */
+	@Test
+	public void testFileFilter() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.filechooser.FileFilter#accept(java.io.File)}.
+	 */
+	@Test
+	public void testAcceptFile1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.filechooser.FileFilter#getDescription()}.
+	 */
+	@Test
+	public void testGetDescription1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
